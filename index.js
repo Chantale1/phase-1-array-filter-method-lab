17bb6ca9fd2e0f0a9c9426bd4bb7f9ae8c0409cd
Chantale <shantelenos99@gmail.com>
2024-03-31 20:30:09 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(Lewi ,Tom) {
+    return Lewi.filter (
+(potentialMatch) =>potentialMatch.toLowerCase() === Tom.toLowerCase ()
+
+    )   
+}
+function fuzzyMatch(Lewi, testString) {
+    return Lewi.filter(
+    (potentialMatch) =>
+    potentialMatch.toLowerCase().indexOf(testString.toLowerCase()) ===0
+    
+    ) 
+}
+
+function matchName(Lewi, TomName) {
+    return Lewi.filter((record) => record.name === TomName );
+    
+}
